84fcf5c03d8036c68965dc8272dbe8f681588bf7
LucasToscano22 <lucas.toscano85@gmail.com>
2026-01-30 18:41:28 -0300

    rf03 e rf07

--- src/main/java/br/com/edu/ifpb/pps/Main.java
+++ src/main/java/br/com/edu/ifpb/pps/Main.java
@@ -1,17 +1,31 @@
 package br.com.edu.ifpb.pps;
 
 import br.com.edu.ifpb.pps.configuracao.Configuracao;
+import br.com.edu.ifpb.pps.model.Anuncio;
+import br.com.edu.ifpb.pps.model.Usuario;
+import br.com.edu.ifpb.pps.moderacao.ModeracaoHandler;
+import br.com.edu.ifpb.pps.moderacao.ModeracaoPreco;
+import br.com.edu.ifpb.pps.moderacao.ModeracaoTermo;
 
 public class Main {
     public static void main(String[] args) {
-    
-        System.out.println("teste rf07");
+        
 
-        Configuracao configuracao = Configuracao.getInstance();
+        Usuario user = new Usuario("João", "joao@email.com");
 
-        System.out.println("Limite de fotos" + configuracao.getLimiteUploadFotos());
-        System.out.println("Taxa de comissao" + configuracao.getTaxaComissao());
-        System.out.println("Termos proibidos" + configuracao.getTermosProbidos());
+        Anuncio anuncioRuim = new Anuncio("Anuncio 1 Legal", "Descricao do anuncio 1", 100, user);
+        Anuncio anuncioBom = new Anuncio("Anuncio 2", "Descricao do anuncio 2", 0, user);
+        
+        ModeracaoHandler validacao = new ModeracaoTermo();
+        validacao.setNext(new ModeracaoPreco());
 
+        System.out.println(" teste anuncio ruim");
+        boolean resultadoRuim = validacao.moderar(anuncioRuim);
+        System.out.println("resultado: " + (resultadoRuim ? "aprovado" : "rejeitado"));
+        
+
+        System.out.println(" teste anuncio bom");
+        boolean resultadoBom = validacao.moderar(anuncioBom);
+        System.out.println("resultado: " + (resultadoBom ? "aprovado" : "rejeitado"));
     }
 }
--- src/main/java/br/com/edu/ifpb/pps/moderacao/ModeradorHandler.java
+++
@@ -1,9 +0,0 @@
-package br.com.edu.ifpb.pps.moderacao;
-
-import br.com.edu.ifpb.pps.model.Anuncio;
-
-public interface ModeradorHandler {
-
-    ModeradorHandler setProximo(ModeradorHandler proximo);
-    boolean moderar(Anuncio anuncio);
-}
