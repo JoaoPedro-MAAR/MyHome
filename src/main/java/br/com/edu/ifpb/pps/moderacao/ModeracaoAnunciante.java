307480c33793fa39a728efcee2d640d4aec291eb
LucasToscano22 <lucas.toscano85@gmail.com>
2026-01-31 18:31:30 -0300

    adiciona handler para verificar se anuncio tem anunciante

---
+++ src/main/java/br/com/edu/ifpb/pps/moderacao/ModeracaoAnunciante.java
@@ -0,0 +1,16 @@
+package br.com.edu.ifpb.pps.moderacao;
+
+import br.com.edu.ifpb.pps.model.Anuncio;
+
+public class ModeracaoAnunciante extends ModeracaoBase {
+
+    @Override
+    public boolean moderar(Anuncio anuncio) {
+        if (anuncio.getAnunciante() == null) {
+            System.out.println("Anúncio não possui anunciante.");
+            return false;
+        }
+
+        return verificarProximo(anuncio);
+    }
+}
